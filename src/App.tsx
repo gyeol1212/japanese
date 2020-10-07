70ae3a235608c2aa910c2bbdcaed8d3cbae7e48c
hangyeol.shin <hangyeol.shin@navercorp.com>
2020-10-07 23:56:43 +0900

    create test site

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {EHiragana, hiragana} from "./hiragana";
+import TestView from "./Test";
+import {EGatakana} from "./gatakana";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [selection, setSelection] = useState<string[]>([])
+    const [start, setStart] = useState<boolean>(false);
+    const [gatakana, setGatakana] = useState<boolean>(false);
+
+    const toggleItems = (e: any) => {
+        const {value} = e.target.dataset;
+        let res;
+
+        if (selection.includes(value)) {
+            res = selection.filter((v: string) => v !== value)
+        } else {
+            res = [...selection, value]
+        }
+
+        setSelection(res)
+        console.log(res)
+    }
+
+    const onStart = () => {
+        setStart(true)
+    }
+
+    const reset = () => {
+        setStart(false)
+        setSelection([])
+    }
+
+    const toggleGatakana = () => {
+        setGatakana(!gatakana);
+    }
+
+    const selectAll = () => {
+        setSelection(Object.keys(hiragana))
+    }
+
+    const resetSelection = () => {
+        setSelection([])
+    }
+
+    return (
+        !start
+            ? <div className="text-center">
+                <div className="h1 my-5">ひらがな / カタカナ</div>
+                <div>
+                    <div className="list-group my-5 mx-auto container p-0">
+                        <div className="row justify-content-end m-0">
+                            <button type="button" onClick={toggleGatakana} className={`btn btn-sm m-2 ${gatakana ? "btn-dark" : "btn-outline-dark" }`}>カタカナ</button>
+                            <button type="button" onClick={selectAll} className="btn btn-sm m-2 mr-2 btn-outline-dark">전체 선택</button>
+                            <button type="button" onClick={resetSelection} className="btn btn-sm m-2  btn-outline-info">전체 취소</button>
+                        </div>
+                        {
+                            Object.keys(hiragana).map((h: string) =>
+                                <div onClick={toggleItems} key={h} data-value={h} className={`list-group-item list-group-item-action list-group-item-light ${selection.includes(h) ? "active" : ""}`}>
+                                    {
+                                        // @ts-ignore
+                                        gatakana? `${EHiragana[h]} / ${EGatakana[h]}`  : `${EHiragana[h]}`
+                                    }
+                                </div>)
+                        }
+                    </div>
+                </div>
+                <button onClick={onStart} type="button" className="btn btn-outline-dark btn-lg btn-block w-50 mx-auto">Start</button>
+            </div>
+            : <TestView selection={selection} reset={reset} gatakana={gatakana}/>
+
+    );
 }
 
 export default App;
---
+++ src/Test.tsx
@@ -0,0 +1,88 @@
+import React, {useEffect, useState} from "react"
+import {hiragana} from "./hiragana";
+import {gatakana} from "./gatakana";
+import {shuffle} from "./util";
+
+interface ITest {
+    selection: string[],
+    reset: any,
+    gatakana: boolean
+}
+
+export default function TestView({selection, reset, gatakana: isGatakana} : ITest) {
+    const [items, setItems] = useState<any>([]);
+    const [target, setTarget] = useState<any>(undefined);
+    const [answer, setAnswer] = useState<boolean>(false);
+
+    useEffect(() => {
+        init()
+    }, [selection])
+
+    const handleKeyDown = (e: React.KeyboardEvent) => {
+        const {key} = e;
+
+        if (key === "Enter") {
+            next();
+        }
+    }
+
+    const next = () => {
+        if (answer) {
+            setAnswer(false)
+            setTarget(items.pop())
+        } else {
+            setAnswer(true)
+        }
+    }
+
+    const init = () => {
+        const allItems: string[] = [];
+
+        setAnswer(false)
+
+        selection.forEach((sel) => {
+            // @ts-ignore
+            allItems.push(...hiragana[sel])
+
+            if (isGatakana) {
+                // @ts-ignore
+                allItems.push(...gatakana[sel])
+            }
+        })
+
+        shuffle(allItems);
+        setItems(allItems);
+        setTarget(allItems.pop())
+    }
+
+    useEffect(() => {
+        // @ts-ignore
+        window.onkeydown = handleKeyDown
+        window.onclick = next
+    })
+
+    const reStart = (e : any) => {
+        e.stopPropagation();
+
+        init()
+    }
+
+
+    return (
+        target
+            ? <div style={{
+                fontSize: "300px"
+            }} className="text-center h1"
+            >
+                <button onClick={reStart} className="d-block btn btn-outline-dark m-5 ml-auto">ReStart</button>
+                {Object.keys(target)[0]}
+                <div style={{
+                    fontSize: "150px"
+                }}>{answer &&  Object.values(target)[0] as string }</div>
+            </div>
+            : <div>
+                <button onClick={init} type="button" className="btn btn-outline-dark btn-lg btn-block w-50 mx-auto mt-5">Re Start</button>
+                <button onClick={reset} type="button" className="btn btn-outline-dark btn-lg btn-block w-50 mx-auto mt-5">Reset</button>
+            </div>
+    )
+}
